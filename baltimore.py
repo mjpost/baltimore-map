a2182304a4962f95c3813af22128b7a93c082459
Matt Post <mattpost@microsoft.com>
2023-10-03 08:15:21 -0400

    Tinkering with grid and other stuff

--- baltimore.py
+++ baltimore.py
@@ -25,7 +25,7 @@ one_km = lat_lon_dist(0.008983, 0.0113636)
 
 
 def main(args):
-    place = args.place
+    place = "Baltimore, MD"
     placename = place.split(',')[0].replace(" ", "").lower()
     # Define a common CRS for both GeoDataFrames (replace with your desired CRS)
     common_crs = 'EPSG:4326'
@@ -69,8 +69,11 @@ def main(args):
     tags = {"leisure": ["park", "garden"]}
     gdf_park = ox.features.features_from_place(place, tags=tags)
     # remove all elements of type node
-    gdf_park = gdf_park[gdf_park["geometry"].apply(lambda x: x.type != "Point")]
+    gdf_park = gdf_park[gdf_park["geometry"].apply(lambda x: x.geom_type != "Point")]
     gdf_park.crs = common_crs
+
+    # write to disk
+    gdf_park.to_csv("baltimore_parks.csv")
 
     # tags = {'boundaries': "administrative", "admin_level": "10"}
     # gdf_neighborhoods = ox.features.features_from_place(place, tags=tags)
@@ -97,21 +100,26 @@ def main(args):
     # gdf_neighborhoods.to_csv("baltimore.csv")
 
     ## Baltimore map
-    fig, ax = plt.subplots(figsize=(24,36))
+    fig, ax = plt.subplots(figsize=(36,48), dpi=300)
 
-    ax.set_xlim(gdf_streets.total_bounds[0] - one_km.x, gdf_streets.total_bounds[2] + one_km.x)
-    ax.set_ylim(gdf_streets.total_bounds[1] - one_km.y * 1.5, gdf_streets.total_bounds[3] + one_km.y * 0.75)
+    # ax.set_xlim(gdf_streets.total_bounds[0] - one_km.x, gdf_streets.total_bounds[2] + one_km.x)
+    # ax.set_ylim(gdf_streets.total_bounds[1] - one_km.y * 1.5, gdf_streets.total_bounds[3] + one_km.y * 0.75)
+
+    ax.set_xlim(gdf_neighborhoods.total_bounds[0] - one_km.x * 0.5, 
+                gdf_neighborhoods.total_bounds[2] + one_km.x * 0.5)
+    ax.set_ylim(gdf_neighborhoods.total_bounds[1] - one_km.y * 0.5, 
+                gdf_neighborhoods.total_bounds[3] + one_km.y * 0.5)
+
+
+    grid_color = "#cccccc"
 
     # print the x and y axis as a faint grid
-    ax.grid(color="#cccccc", linestyle="--", linewidth=0.5)
+    ax.grid(color=grid_color, linestyle="--", linewidth=0.5)
 
     # put the axis labels to the top and to the right
     ax.xaxis.tick_top()
     ax.yaxis.tick_right()
-
-    # put tick marks outside the plot
-    ax.tick_params(axis="both", direction="out", length=6, width=2, color="#cccccc")
-    # ax.tick_params(axis="both", direction="in")
+    ax.tick_params(axis="both", direction="in", length=6, width=0.5, colors=grid_color)
 
     # use three decimal places for the axis tick labels
     ax.xaxis.set_major_formatter(plt.FormatStrFormatter("%.3f"))
@@ -126,7 +134,7 @@ def main(args):
 
     # use a dashed line for the axis grid
     gdf_streets.plot(ax=ax, ec=street_color, linewidth=1, alpha=0.5)
-    gdf_water.plot(ax=ax, facecolor=water_blue, ec=water_blue, linewidth=1, alpha=0.3)
+    gdf_water.plot(ax=ax, facecolor=water_blue, ec=water_blue, linewidth=1, alpha=0.5)
     gdf_park.plot(ax=ax, facecolor=park_green, ec="black", linewidth=1.2, alpha=0.3)
     gdf_buildings.plot(ax=ax, facecolor=cemetery_gray, linewidth=1.2, alpha=0.3)
     # gdf_neighborhoods["color"] = gdf_neighborhoods.apply(lambda x: "#%06x" % random.randint(0, 0xFFFFFF), axis=1)
@@ -138,18 +146,19 @@ def main(args):
 
     font_color = "#aaaaaa"
 
-    # BALTIMORE
-    ax.text(
-        s="Baltimore",
-        x=gdf_neighborhoods.total_bounds[0],
-        y=gdf_neighborhoods.total_bounds[1] - 0.003,  # + (gdf_streets.total_bounds[3] - gdf_streets.total_bounds[1]) * 0.25,
-        fontsize=144,
-        color=font_color,
-        weight="bold",
-        verticalalignment="bottom",
-        horizontalalignment="left",
-        name="Phosphate",
-    )
+    # BALTIMORE city name
+    if False:
+        ax.text(
+            s="Baltimore",
+            x=gdf_neighborhoods.total_bounds[0],
+            y=gdf_neighborhoods.total_bounds[1] - 0.003,  # + (gdf_streets.total_bounds[3] - gdf_streets.total_bounds[1]) * 0.25,
+            fontsize=144,
+            color=font_color,
+            weight="bold",
+            verticalalignment="bottom",
+            horizontalalignment="left",
+            name="Phosphate",
+        )
 
     # # NEIGHBORHOODS 2023
     # ax.text(
@@ -195,23 +204,49 @@ def main(args):
         x = row["geometry"].centroid.x + offsets.get(row["Name"], (0, 0))[0]
         y = row["geometry"].centroid.y + offsets.get(row["Name"], (0, 0))[1]
 
-    ax.annotate(
-        text=munge(row["Name"]),
-        xy=(x, y),
-        horizontalalignment="center",
-        verticalalignment="center",
-        fontsize=6.5,
-        color="#999999",
-        weight="bold",
-        # name="Avenir Next Condensed",
-        name="Phosphate",
-    )
+        ax.annotate(
+            text=munge(row["Name"]),
+            xy=(x, y),
+            horizontalalignment="center",
+            verticalalignment="center",
+            fontsize=6.5,
+            color="#999999",
+            weight="bold",
+            name="Avenir Next Condensed",
+            # name="Phosphate",
+        )
+
+    # for every relation in parks, print the name in the middle of it
+    if False:
+        park_count = 0
+        for idx, row in gdf_park.iterrows():
+            name = row["name"]
+
+            if name == "" or name is float:
+                continue
+
+            park_count += 1
+
+            x = row["geometry"].centroid.x
+            y = row["geometry"].centroid.y
+
+            print(park_count, "PARK", name, x, y)
+
+            ax.annotate(
+                text=row["name"],
+                xy=(x, y),
+                horizontalalignment="center",
+                verticalalignment="center",
+                fontsize=6.5,
+                color="#999999",
+                style="italic",
+                name="Avenir Next Condensed",
+            )
 
     plt.savefig(f"{placename}.pdf", dpi=300)
 
 
 def draw_legend(ax, x, y):
-
     wx = one_km.x / 3
     wy = one_km.y / 3
 
@@ -326,9 +361,7 @@ def draw_scale_patch(ax, x, y):
         name="Avenir Next",
     )
 
-    ##
-    ## COMPASS
-    ##
+
 def draw_compass(ax, x, y):
     # Draw an arrow pointing north, with a fancy N above it
     ax.text(
@@ -359,7 +392,6 @@ def draw_compass(ax, x, y):
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("--place", "-p", help="City on earth", default="Baltimore, MD, USA")
     args = parser.parse_args()
 
     main(args)
